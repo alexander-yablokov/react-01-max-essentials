8bddc4a683b7722480c5968c0c55c359a38eb33c
Alexander Yablokov <tajikistan@rambler.ru>
2024-09-27 14:28:34 +0500

    Conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,22 @@ import TabButton from "./components/TabButton";
 import {CORE_CONCEPTS, EXAMPLES} from "./data";
 
 function App() {
-    const [selectedTopic, setSelectedTopic] = useState('components');
+    const [selectedTopic, setSelectedTopic] = useState();
 
     // функция-обработчик должна быть внутри компонента
     function handleClick(selectButton) {
         setSelectedTopic(selectButton);
+    }
+
+    let tabContent = <p>Please select a topic.</p>;
+    if (selectedTopic) {
+        tabContent = (
+            <div id="tab-content">
+                <h3>{EXAMPLES[selectedTopic].title}</h3>
+                <p>{EXAMPLES[selectedTopic].description}</p>
+                <pre><code>{EXAMPLES[selectedTopic].code}</code></pre>
+            </div>
+        );
     }
 
     return (
@@ -43,12 +54,7 @@ function App() {
                         <TabButton onClick={() => handleClick('props')}>Props</TabButton>
                         <TabButton onClick={() => handleClick('state')}>State</TabButton>
                     </menu>
-                    <div id="tab-content">
-                        <h3>{EXAMPLES[selectedTopic].title}</h3>
-                        <p>{EXAMPLES[selectedTopic].description}</p>
-                        <pre><code>{EXAMPLES[selectedTopic].code}</code></pre>
-                    </div>
-
+                    {tabContent}
                 </section>
             </main>
         </div>
