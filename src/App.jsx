c476b0b8b8fb89e2dcbd14f54949ac5a5168c17e
Alexander Yablokov <tajikistan@rambler.ru>
2024-09-27 14:23:00 +0500

    Deriving data based on state

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import {useState} from "react";
 import Header from './components/Header/Header';
 import CoreConcept from "./components/CoreConcept";
 import TabButton from "./components/TabButton";
-import {CORE_CONCEPTS} from "./data";
+import {CORE_CONCEPTS, EXAMPLES} from "./data";
 
 function App() {
-    const [selectedTopic, setSelectedTopic] = useState('Please click a button');
+    const [selectedTopic, setSelectedTopic] = useState('components');
 
     // функция-обработчик должна быть внутри компонента
     function handleClick(selectButton) {
@@ -43,7 +43,12 @@ function App() {
                         <TabButton onClick={() => handleClick('props')}>Props</TabButton>
                         <TabButton onClick={() => handleClick('state')}>State</TabButton>
                     </menu>
-                    {selectedTopic}
+                    <div id="tab-content">
+                        <h3>{EXAMPLES[selectedTopic].title}</h3>
+                        <p>{EXAMPLES[selectedTopic].description}</p>
+                        <pre><code>{EXAMPLES[selectedTopic].code}</code></pre>
+                    </div>
+
                 </section>
             </main>
         </div>
